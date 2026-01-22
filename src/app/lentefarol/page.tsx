ade9606a10ad004740a8cf50b58ea3693b825f17
thainabsouza <thainasouza.sud@gmail.com>
2026-01-22 03:26:30 -0300

    fix: remove puppeteer from pages

--- src/app/lentefarol/page.tsx
+++ src/app/lentefarol/page.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useCart } from "@/context/CartContext";
-import { launch } from "puppeteer";
+
 
 type Lentefarol = {
   id: number;
